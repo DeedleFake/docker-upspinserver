b2e134a2b9d6ea7010e2d03582be64528f34de06
DeedleFake <deedlefake@users.noreply.github.com>
2017-02-23 19:09:37 -0500

    Build Docker image.

--- build.bash
+++ build.bash
@@ -1,7 +1,14 @@
 #!/usr/bin/bash
 
+name="$1"
+if [[ -z "$name" ]]; then
+	name="deedlefake/upspinserver"
+fi
+
 vol="$(cd "$(dirname "$BASH_SOURCE")" && pwd)"
 echo "Building in $vol..."
 
 docker run --rm -e CGO_ENABLED=0 -v "$vol:/go" golang go get -u upspin.io/cmd/upspinserver
 cp "$vol/bin/upspinserver" .
+
+docker build -t "$name" .
